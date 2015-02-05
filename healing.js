1897dd42cc232726069a2e4130c605d65cd8fb0b
docclock <spiderfingers@web.de>
2015-02-05 13:40:22 +0100

    Update healing.js

--- healing.js
+++ healing.js
@@ -1,17 +1,23 @@
 module.exports = function (creep) {
      
- 	if(creep.energy == 0) {
-	    	creep.moveTo(Game.spawns.Spawn1);
-		    Game.spawns.Spawn1.transferEnergy(creep);
-    }
+ //	if(creep.energy == 0) {
+	 //   creep.moveTo(Game.spawns.Spawn1);
+	//	Game.spawns.Spawn1.transferEnergy(creep);
+   // }
     	
     for(var i in Game.creeps) {
     
 	  var target = Game.creeps[i];
     
-      if(target.my && target.hits < target.hitsMax && target!= creep) {
+      if(target.my && target.hits < target.hitsMax && target!= creep &&
+      target.memory.role=='guarding') {
         creep.moveTo(target);
         creep.heal(target);
       }
+    }
+    
+    if(creep.getActiveBodyparts(Game.HEAL) == 0 ||
+        creep.getActiveBodyparts(Game.MOVE) == 0 ){
+        creep.suicide();
     }
 }
